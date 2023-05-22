0c2625e0f81830e4cbddd7efb7814d4136a8f2e0
Cmeesh11 <cartermeesh11@gmail.com>
2023-05-22 18:40:33 -0400

    portfolio page renders images

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,16 +6,16 @@ function Header(props) {
     <span className="navbar-brand navbar-text">Carter Michaud</span>
       <ul className="navbar-nav ms-auto mb-2 d-flex flex-sm-row justify-content-between align-items-center">
         <li className="nav-item mx-1">
-          <a className="nav-link navbar-text" onClick={() => props.handlePageChange('about')} href="#">About me</a>
+          <a className="nav-link navbar-text" onClick={() => props.handlePageChange('about')} href="#about">About me</a>
         </li>
         <li className="nav-item mx-1">
-          <a className="nav-link navbar-text" onClick={() => props.handlePageChange('portfolio')} href="#">Portfolio</a>
+          <a className="nav-link navbar-text" onClick={() => props.handlePageChange('portfolio')} href="#portfolio">Portfolio</a>
         </li>
         <li className="nav-item mx-1">
-          <a className="nav-link navbar-text" onClick={() => props.handlePageChange('contact')} href="#">Contact</a>
+          <a className="nav-link navbar-text" onClick={() => props.handlePageChange('contact')} href="#contact">Contact</a>
         </li>
         <li className="nav-item mx-1">
-          <a className="nav-link navbar-text" onClick={() => props.handlePageChange('resume')} href="#">Resume</a>
+          <a className="nav-link navbar-text" onClick={() => props.handlePageChange('resume')} href="#resume">Resume</a>
         </li>
       </ul>
   </div>
--- src/components/nav-components/Portfolio.js
+++ src/components/nav-components/Portfolio.js
@@ -1,8 +1,43 @@
+import dailyPlanner from '../../assets/images/daily-planner-img.png';
+import ecommerce from '../../assets/images/E-Commerce.png';
+import inneed from '../../assets/images/inneed.png';
+import javascriptQuiz from '../../assets/images/javascript_quiz.png';
+import marvel from '../../assets/images/Marvel-Typing-Trainer.png';
+import techBlog from '../../assets/images/Tech-blog.png';
+import weatherForecast from '../../assets/images/weather_forecast.png';
+
+const style = {
+  card: {
+    width: '18.6rem',
+  }
+}
 
 function Portfolio() {
   return(
-    <div className="card">
-      <img src="../../assets/images/daily-planner-img.png" alt="daily planner" className="card-img"/>
+    <div className='container'>
+      <div className='row'>
+        <div className="card m-3" style={style.card}>
+          <img src={dailyPlanner} alt="daily planner" className="card-img"/>
+        </div>
+        <div className="card m-3" style={style.card}>
+          <img src={ecommerce} alt="ecommerce" className="card-img"/>
+        </div>
+        <div className="card m-3" style={style.card}>
+          <img src={inneed} alt="inneed" className="card-img"/>
+        </div>
+        <div className="card m-3" style={style.card}>
+          <img src={javascriptQuiz} alt="Javascript Quiz" className="card-img"/>
+        </div>
+        <div className="card m-3" style={style.card}>
+          <img src={marvel} alt="Marvel Typing Trainer" className="card-img"/>
+        </div>
+        <div className="card m-3" style={style.card}>
+          <img src={techBlog} alt="Tech Blog" className="card-img"/>
+        </div>
+        <div className="card m-3" style={style.card}>
+          <img src={weatherForecast} alt="Weather Forecast" className="card-img"/>
+        </div>
+      </div>
     </div>
   );
 }
