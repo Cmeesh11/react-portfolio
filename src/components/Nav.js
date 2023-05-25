ab50c1a38653ba67382550e6f456df88bb847a9f
Cmeesh11 <cartermeesh11@gmail.com>
2023-05-25 18:30:24 -0400

    Updates

--- src/components/Nav.js
+++
@@ -1,9 +0,0 @@
-
-function Nav() {
-  return(
-    <h1>Hello world</h1>
-  );
-}
-
-
-export default Nav;
--- src/components/nav-components/Resume.js
+++
@@ -1,8 +0,0 @@
-
-function Resume() {
-  return(
-    <h3 className="text-center mt-5"><a href='../../assets/carter-michaud-resume.pdf' download>Download my resume!</a></h3>
-  );
-}
-
-export default Resume;
